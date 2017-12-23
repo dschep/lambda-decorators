b727fb037d4599f64cd735aec0b749ef069b7c13
Daniel Schep <dschep@gmail.com>
2017-12-23 11:37:55 -0500

    github banner

--- conf.py
+++ conf.py
@@ -96,7 +96,9 @@ html_theme = 'alabaster'
 # further.  For a list of options available for each theme, see the
 # documentation.
 #
-# html_theme_options = {}
+html_theme_options = {'github_user': 'dschep',
+                      'github_repo': 'lambda-decorators',
+                      'github_banner': True}
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
