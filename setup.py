7a8ae8bf44cee012c1f89850e9d6e93f1334b10e
Daniel Schep <dschep@gmail.com>
2017-12-23 13:08:48 -0500

    setup.py!

---
+++ setup.py
@@ -0,0 +1,37 @@
+from setuptools import setup
+
+long_description = open('README.rst').read()
+
+setup(
+    name='lambda-decorators',
+
+    version='0.1a',
+
+    description='A collection of useful decorators for making AWS Lambda handlers',
+    long_description=long_description,
+
+    url='https://github.com/dschep/lambda-decorators',
+
+    author='Daniel Schep',
+    author_email='dschep@gmail.com',
+
+    license='MIT',
+
+    # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
+    classifiers=[
+        'Development Status :: 4 - Alpha',
+
+        'Environment :: Serverless',
+
+        'Intended Audience :: Developers',
+
+        'License :: OSI Approved :: MIT',
+
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+    ],
+
+    keywords='serverless lambda decorator aws',
+
+    py_modules='lambda_decorator',
+)
