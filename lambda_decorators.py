0b66d425d6fbcd302dfe4d6bbd72c0e0b276a75b
Daniel Schep <dschep@gmail.com>
2018-01-05 18:25:33 -0500

    better words about jsonschema

--- lambda_decorators.py
+++ lambda_decorators.py
@@ -485,8 +485,9 @@ def json_schema_validator(request_schema=None, response_schema=None):
     Validate your request & response payloads against a JSONSchema.
 
     *NOTE: depends on the* `jsonschema <https://github.com/Julian/jsonschema>`_
-    *package. If installing lambda_decorators via the cURL method, this
-    decorator won't work.*
+    *package. If you're using* `serverless-python-requirements <https://github.com/UnitedIncome/serverless-python-requirements>`_
+    *you're all set. If you cURLed* ``lambda_decorators.py`` *you'll have to
+    install it manually in your service's root directory.*
 
     Usage::
 
