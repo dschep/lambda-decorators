836fecd7355fbe857cba2f041dcbcd3551ba1f54
Daniel Schep <dschep@gmail.com>
2018-02-04 11:56:32 -0500

    a bit more test covg

---
+++ tests/test_json_schema_validator.py
@@ -0,0 +1,54 @@
+from lambda_decorators import json_schema_validator
+
+
+schema = {
+    'type': 'object',
+    'properties': {
+        'price': {'type': 'number'},
+    },
+}
+
+
+def test_request_schema_success():
+    @json_schema_validator(request_schema=schema)
+    def handler(event, context):
+        return event['price']
+
+    assert handler({'price': 1}, object()) == 1
+
+
+def test_request_schema_error():
+    @json_schema_validator(request_schema=schema)
+    def handler(event, context):
+        return event['price']
+
+    assert handler({'price': 'foo'}, object()) == {
+        'statusCode': 400,
+        'body': "RequestValidationError: 'foo' is not of type 'number'",
+    }
+
+
+def test_response_schema_success():
+    @json_schema_validator(response_schema=schema)
+    def handler(event, context):
+        return {'price': 1}
+
+    assert handler({}, object()) == {'price': 1}
+
+
+def test_response_schema_error():
+    @json_schema_validator(response_schema=schema)
+    def handler(event, context):
+        return {'price': 'foo'}
+
+    assert handler({}, object()) == {
+        'statusCode': 500,
+        'body': "ResponseValidationError: 'foo' is not of type 'number'",
+    }
+#
+#{'statusCode': 400, 'body': "RequestValidationError: 'bar' is not of type 'number'"}
+#@json_schema_validator()
+#def handler(event, context):
+#    return {'price': 'bar'}
+#handler({}, object())
+#{'statusCode': 500, 'body': "ResponseValidationError: 'bar' is not of type 'number'"}
